39e9c469a2654b6d8d8f09a8492fb0ce71a6c503
Tatiana Korol <tatiana.korol@student.ehu.lt>
2024-10-13 23:18:23 +0200

    feat: add custom styles

--- main.js
+++ main.js
@@ -14,11 +14,24 @@ class Heading {
 class Button {
     #node;
 
-    constructor(content, className) {
+    constructor(content, className, style = {}) {
         this.#node = document.createElement('button');
         this.#node.setAttribute('class', 'button');
         this.#node.classList.add(className);
         this.#node.textContent = content;
+        if (className = 'btn--card') {
+            this.#customizeBtn(style);
+        }
+    }
+
+    #customizeBtn(props) {
+        let {_isDefault, _backgroundColor, _textColor} = props;
+        if (_isDefault) {
+            this.#node.classList.add('btn-default--card');
+        } else {
+            this.#node.style.backgroundColor = _backgroundColor;
+            this.#node.style.color = _textColor;
+        }
     }
 
     get node() {
@@ -52,23 +65,38 @@ class Card {
         this.#node.classList.add('card');
         this.#renderHeading(props);
         this.#renderParagraph(props);
-        this.#renderBtn();
+        this.#renderBtn(props);
+        this.#renderBackground(props);
     }
 
     #renderHeading(props) {
-        const h5 = new Heading('h5', props.heading);
+        const h5 = new Heading('h5', props.heading.content);
+        h5.node.style.color = props.heading._color;
         this.#node.append(h5.node);
     }
 
     #renderParagraph(props) {
         const p = document.createElement('p');
-        p.textContent = props.text;
+        p.textContent = props.text.content;
+        p.style.color = props.text._color;
         this.#node.append(p);
     }
 
-    #renderBtn() {
-        const btn = new Button('Explore', 'btn--card');
+    #renderBtn(props) {
+        const btn = new Button('Explore', 'btn--card', props._button);
         this.#node.append(btn.node);
+    }
+
+    #renderBackground(props) {
+        if (props._background._color) {
+            this.#node.style.backgroundColor = props._background._color;
+            if (props._background._color === '#FFFFFF') {
+                this.#node.style.border = '2px solid #EBEAED';
+            }
+        }
+        if (props._background._img) {
+            this.#node.style.backgroundImage = props._background._img;
+        }
     }
 
     get node() {
@@ -78,15 +106,19 @@ class Card {
 
 function Page(props) {
     const section = document.createElement('section');
+
     const header = document.createElement('div');
     header.setAttribute('class', 'header');
+
     const h1 = new Heading('h1', 'Last works');
     const button = new Button('Explore Showcase', 'btn--header');
     const grid = new Grid();
     const cards = props.map((item) => new Card(item));
+
     cards.forEach((card) => grid.node.append(card.node));
     header.append(h1.node, button.node);
     section.append(header, grid.node);
+
     return section;
 }
 
@@ -98,20 +130,70 @@ function renderPage(data) {
 function loadData() {
     return [
         {
-            heading: 'Startup Framework',
-            text: 'Startup is a powerful tool for quick and convenient proto-typing of your projects. It will fit most projects because it contains up-to-date and modern web elements.'
+            heading: {
+                content: 'Startup Framework',
+                _color: '#1E0E62',
+            },
+            text: {
+                content: 'Startup is a powerful tool for quick and convenient proto-typing of your projects. It will fit most projects because it contains up-to-date and modern web elements.',
+                _color: '#1E0E62',
+            },
+            _button: {
+                _isDefault: true,
+            },
+            _background: {
+                _color: '#EBEAED',
+            }
         },
         {
-            heading: 'Web Generator',
-            text: 'Startup is a powerful tool for quick and convenient proto-typing of your projects. It will fit most projects because it contains up-to-date and modern web elements.'
+            heading: {
+                content: 'Web Generator',
+                _color: '#1E0E62',
+            },
+            text: {
+                content: 'Startup is a powerful tool for quick and convenient proto-typing of your projects. It will fit most projects because it contains up-to-date and modern web elements.',
+                _color: 'rgba(21, 20, 57, 0.4)',
+            },
+            _button: {
+                _isDefault: false,
+                _backgroundColor: '#25DAC5',
+                _textColor: '#FFFFFF',
+            },
+            _background: {
+                _color: '#FFFFFF',
+            }
         },
         {
-            heading: 'Slides 4',
-            text: 'All of these components are made in the same style, and can easily be inegrated into projects, allowing you to create hundreds of solutions for your future projects.'
+            heading: {
+                content: 'Slides 4',
+                _color: '#FFFFFF'
+            },
+            text: {
+                content: 'All of these components are made in the same style, and can easily be inegrated into projects, allowing you to create hundreds of solutions for your future projects.',
+                _color: '#FFFFFF',
+            },
+            _button: {
+                _isDefault: true,
+            },
+            _background: {
+                _color: '#482BE7',
+            }
         },
         {
-            heading: 'Postcards',
-            text: 'All frequently used elements are now in symbols. Use them to create interfaces really fast. Easily change icons, colors and text. Add new symbols to customize your design.'
+            heading: {
+                content: 'Postcards',
+                _color: '#FFFFFF',
+            },
+            text: {
+                content: 'All frequently used elements are now in symbols. Use them to create interfaces really fast. Easily change icons, colors and text. Add new symbols to customize your design.',
+                _color: '#FFFFFF',
+            },
+            _button: {
+                _isDefault: true,
+            },
+            _background: {
+                _img: 'url("./assets/postcards_background.png")',
+            }
         }
     ];
 }
