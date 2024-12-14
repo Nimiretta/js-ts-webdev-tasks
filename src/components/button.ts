c1a77c8950dac07e5222c841545ae08f6ecf73ae
yanitaBiheza <yanita.biheza@student.ehu.lt>
2024-12-14 21:31:17 +0300

    delete button component, switch branch

--- src/components/button.ts
+++
@@ -1,7 +0,0 @@
-export function CreateButton(label: string, classparam: string): string {
-    const button = `
-    <button type="button" class="button ${classparam}>${label}</button>
-    `;
-
-    return button;
-}
