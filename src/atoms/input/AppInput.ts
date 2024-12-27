493ea99965866d82f2e3d926c81515827dd7a0ea
Anna Kudriashova <amarenoru@gmail.com>
2024-12-27 21:05:07 +0200

    fix: replace hard-coded style

--- src/atoms/input/AppInput.ts
+++ src/atoms/input/AppInput.ts
@@ -8,21 +8,33 @@ export function AppInputAtom({
     icon,
 }: TInput): HTMLDivElement {
     const wrapper = document.createElement('div');
-    wrapper.className = 'flex items-center rounded-5xl p-3 bg-white';
-    wrapper.classList.add(...wrapperClasses);
+    wrapper.classList.add(
+        'flex',
+        'items-center',
+        'rounded-5xl',
+        'p-3',
+        'bg-white',
+        ...wrapperClasses
+    );
 
     const input = document.createElement('input');
     input.type = type;
     input.placeholder = placeholder;
-    input.className =
-        'w-full border-none outline-none text-gray-500 text-sm bg-transparent placeholder-gray-400 focus:outline-none';
-    input.classList.add(...inputClasses);
+    input.classList.add(
+        'w-full',
+        'border-none',
+        'outline-none',
+        'text-gray-500',
+        'text-sm',
+        'bg-transparent',
+        'placeholder-gray-400',
+        'focus:outline-none',
+        ...inputClasses
+    );
 
     if (icon) {
+        input.classList.add('bg-left-3', 'bg-no-repeat', 'pl-10');
         input.style.backgroundImage = `url('${icon}')`;
-        input.style.backgroundRepeat = 'no-repeat';
-        input.style.backgroundPosition = 'left 12px center';
-        input.classList.add('pl-10');
     }
 
     wrapper.appendChild(input);
