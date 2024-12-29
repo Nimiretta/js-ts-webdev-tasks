9c84e3028d3d46c04021dcbc72443cff86aa4980
Tatiana Korol <tatiana.korol@student.ehu.lt>
2024-12-29 16:07:32 +0100

    Revert "feat: update eslint config"
    This reverts commit 2d4ad1e6735b106b9d2212407a1069225de32871.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,15 +4,8 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import prettierConfig from "eslint-config-prettier";
 
-export default [
-  tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
-    prettierConfig,
-  ),
-  {
-    rules: {
-      '@typescript-eslint/explicit-function-return-type': 'error',
-    },
-  },
-];
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  prettierConfig,
+);
