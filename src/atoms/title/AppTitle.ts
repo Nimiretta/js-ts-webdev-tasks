6423bd9d13bb4c2427b9d32f753086cb7b1187f0
Anna Kudriashova <amarenoru@gmail.com>
2025-01-05 18:08:55 +0200

    fix: fix on comments

--- src/atoms/title/AppTitle.ts
+++ src/atoms/title/AppTitle.ts
@@ -3,10 +3,16 @@ import { TitleTag, TTitle } from '../../types';
 export function AppTitle({
     tag = TitleTag.H1,
     textContent = '',
+    innerHTML = '',
     classes = [],
 }: TTitle): HTMLElement {
     const title = document.createElement(tag);
-    title.textContent = textContent;
+
+    if (innerHTML) {
+        title.innerHTML = innerHTML;
+    } else {
+        title.textContent = textContent;
+    }
 
     title.classList.add('font-bold', 'text-black', ...classes);
 
