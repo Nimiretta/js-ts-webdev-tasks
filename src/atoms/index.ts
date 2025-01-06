fb73db76479ff4e6ee38b60adf512429000ec005
Tatiana Korol <79996554+Nimiretta@users.noreply.github.com>
2025-01-06 20:21:28 +0100

    feat: add labelLink atom (#7)
    * feat: add labelLink atom

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -3,4 +3,5 @@ export * from './counter/AppCounter';
 export * from './price/AppPrice';
 export * from './rating/AppRating';
 export * from './filterOption/AppFilterOption';
+export * from './labelLink/AppLabelLink';
 export * from './title/AppTitle';
