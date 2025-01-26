e221bbec9718b11169dd8fe48d142e8e85399e85
Anna Kudriashova <60935470+AnnaKudryashova@users.noreply.github.com>
2025-01-26 15:43:24 +0200

    feat: add product image gallery (#21)
    * feat: add product image gallery
    
    * fix: fix on comments
    
    * fix: delete buildStyle
    
    ---------
    
    Co-authored-by: Anna Kudriashova <amarenoru@gmail.com>

--- src/molecules/index.ts
+++ src/molecules/index.ts
@@ -1,3 +1,4 @@
 export * from './breadcrumbs/AppBreadcrumbs';
 export * from './linkSection/AppLinkSection';
 export * from './card/AppCard';
+export * from './productImageGallery/AppProductImageGallery';
