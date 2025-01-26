0375de5ed8bef459ee1d92c204ac065a568fcf9a
yanitaBiheza <yanita.biheza@student.ehu.lt>
2025-01-27 00:12:47 +0300

    feat: start logoString fn

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,9 @@ import router from './router';
 document.addEventListener('DOMContentLoaded', () => {
     router.resolve();
 });
+
+import { AppLogoString } from './molecules/logoString/AppLogoString';
+
+const str = AppLogoString();
+document.body.append(str);
+console.log('ghfc');
---
+++ src/molecules/logoString/AppLogoString.ts
@@ -0,0 +1,38 @@
+import { AppLogo } from '../../atoms';
+import { AppIconLink } from '../../atoms';
+
+export function AppLogoString(): HTMLElement {
+    const logoStr = document.createElement('div');
+    const logo = AppLogo();
+    logoStr.append(logo);
+    const iconsBlock = document.createElement('div');
+    iconsBlock.classList.add('flex');
+    const cartIcon = AppIconLink({
+        icon: '../../assets/icons/cart.svg',
+        linkValue:
+            'https://github.com/Nimiretta/js-ts-webdev-tasks/pull/21/files',
+        width: 'w-[24px]',
+        height: 'w-[24px]',
+    });
+    const profIcon = AppIconLink({
+        icon: '../../assets/icons/profile.svg',
+        linkValue:
+            'https://github.com/Nimiretta/js-ts-webdev-tasks/pull/21/files',
+        width: 'w-[24px]',
+        height: 'w-[24px]',
+    });
+    iconsBlock.append(profIcon);
+    iconsBlock.append(cartIcon);
+    logoStr.append(iconsBlock);
+
+    logoStr.classList.add(
+        'flex',
+        'flex-row',
+        'justify-between',
+        'w-[86%]',
+        'border',
+        'my-9'
+    );
+
+    return logoStr;
+}
