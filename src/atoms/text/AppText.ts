899f88abdd2e9bd4041253696e131dfe2914a394
yanitaBiheza <yanita.biheza@student.ehu.lt>
2025-01-29 00:01:11 +0300

    fix: fix color class

--- src/atoms/text/AppText.ts
+++ src/atoms/text/AppText.ts
@@ -5,7 +5,7 @@ export function AppText({
     textContent = '',
     innerHTML = '',
     classes = [],
-    textColor = '#00000099',
+    textColor = 'text-text-primary',
 }: TText): HTMLElement {
     const textElement = document.createElement(innerHTML ? 'div' : tag);
 
