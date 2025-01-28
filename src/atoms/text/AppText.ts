57eb7ef321a8b44fb5ee8aa519e3a8697a2ce473
yanitaBiheza <yanita.biheza@student.ehu.lt>
2025-01-28 23:40:46 +0300

    fix: change default  text color in AppText

--- src/atoms/text/AppText.ts
+++ src/atoms/text/AppText.ts
@@ -5,7 +5,7 @@ export function AppText({
     textContent = '',
     innerHTML = '',
     classes = [],
-    textColor = 'text-discount-gray',
+    textColor = '#00000099',
 }: TText): HTMLElement {
     const textElement = document.createElement(innerHTML ? 'div' : tag);
 
