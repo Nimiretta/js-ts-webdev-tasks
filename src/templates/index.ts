1b037dc72182db623dc0f7334a451316082a18f6
Tatiana Korol <tatiana.korol@student.ehu.lt>
2025-02-04 02:44:38 +0100

    feat: add baseTemplate

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -2,3 +2,4 @@ export * from './orderFlow/AppOrderFlow';
 export * from './productDetail/AppProductDetail';
 export * from './orderConfirmation/AppOrderConfirmation';
 export * from './category/AppCategory';
+export * from './baseTemplate/AppBaseTemplate';
