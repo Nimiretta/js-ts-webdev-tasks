53a4faef0a5472f27b82bc8b06c1d82e0b922eeb
Tatiana Korol <tatiana.korol@student.ehu.lt>
2025-02-04 02:59:37 +0100

    feat: implement router with mock instead of baseTemplate

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,32 @@ import Navigo from 'navigo';
 
 const router = new Navigo('/');
 
+function handleAsyncRouteChange(handler: (params?: unknown) => Promise<HTMLElement>, params?: unknown) {
+    const newApp = document.createElement('div');
+    newApp.setAttribute('id', 'app');
+    document.body.append(newApp);
+    const app = document.getElementById('app');
+    if (app) {
+        handler(params).then((page) => {
+            app.innerHTML = '';
+            app.append(page);
+        })
+    }
+}
+
+function handleSyncRouteChange(handler: (params?: unknown) => HTMLElement, params?: unknown) {
+    const newApp = document.createElement('div');
+    newApp.setAttribute('id', 'app');
+    document.body.append(newApp);
+    const app = document.getElementById('app');
+    if (app) {
+        const page = handler(params);
+        app.innerHTML = '';
+        app.append(page);
+        
+    }
+}
+
 router
     .on({
         '/': () => {},
