89492b0dde4b193328107a55e6d8d24ff69b52ce
Tatiana Korol <79996554+Nimiretta@users.noreply.github.com>
2025-02-05 22:35:44 +0100

    Update src/organisms/header/AppHeader.ts
    Co-authored-by: Eugenia <90528280+eu-peregudova@users.noreply.github.com>

--- src/organisms/header/AppHeader.ts
+++ src/organisms/header/AppHeader.ts
@@ -4,7 +4,7 @@ export function AppHeader(cartId: string | number): HTMLDivElement {
     const header = document.createElement('div');
     header.setAttribute('id', 'header');
     const signupWrapper = document.createElement('div');
-    signupWrapper.classList.add('bg-black', 'w-[90rem]', 'mx-auto');
+    signupWrapper.classList.add('bg-black', 'w-dvw');
     const signupBanner = AppSignupBanner();
     signupWrapper.append(signupBanner);
 
